0314a844b4f9fe8a2e16db6c8493e9611837af1b
kishorekalapala <kvn.kishore24@gmail.com>
2015-05-14 13:19:52 +0530

    Readme file

---
+++ astest/src/testcases/Readme.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class Readme {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Welcome to DN");
+	  System.out.println("Alpha Street");
+  }
+}
